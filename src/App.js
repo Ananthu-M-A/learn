7c05e3209fdef4aaf11a0f321c91c6c434717361
Ananthu M A <heartchaserananthu@gmail.com>
2023-10-25 12:41:46 +0530

    Counter & Data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import {useState} from 'react';
-import Counter from './Counter';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+function CounterWithUserInfo() {
   const [count, setCount] = useState(0);
-  const plus = () => {
-    setCount(count + 1);
-  }
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const minus = () => {
-    setCount(count - 1);
-  }
+  const fetchUserInformation = async (count) => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${count}`);
+      setUser(response.data);
+      setLoading(false);
+    } catch (err) {
+      setError("Error... Try counting 1 to 10...");
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserInformation(count);
+  }, [count]);
 
   return (
-    <div className="container-fluid text-center p-5">
-      <button className="btn border rounded-3 text-light bg-dark" onClick = {plus}> + </button>
-      <Counter count = {count} />
-      <button className="btn border rounded-3 text-light bg-dark" onClick = {minus}> - </button>
+    <div className="container-fluid text-center mt-3">
+      <div className="row">
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+          <h1 className="bg-primary text-light">Counter: {count}</h1>
+          {loading ? (
+            <h2>Loading...</h2>
+          ) : error ? (
+            <p className="text-dark bg-danger">{error}</p>
+          ) : user ? (
+            <div className="bg-success p-3 text-light">
+              <h2>User Information:</h2>
+              <p>Name: {user.name}</p>
+              <p>Email: {user.email}</p>
+              <p>Phone: {user.phone}</p>
+            </div>
+          ) : null}
+
+        </div>
+        <div className="col-md-4"></div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+          <button className="btn btn-primary mx-2" onClick={() => setCount(count - 1)}>PREV</button>
+          <button className="btn btn-primary mx-2" onClick={() => setCount(count + 1)}>NEXT</button>
+        </div>
+        <div className="col-md-4"></div>
+      </div>
     </div>
+
   );
 }
 
-export default App;
+export default CounterWithUserInfo;
